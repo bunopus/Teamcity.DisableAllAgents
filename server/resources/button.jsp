20bcc2ed3d0e4ec6057d7e7ba9434073453a8015
Evgeny Gusev <bunopus@gmail.com>
2015-05-01 21:34:03 +0300

    1. Added bean to xml. Important to not forget init-method attribute 2. Added initional feature - add two links on agent page
    Signed-off-by: Evgeny Gusev <bunopus@gmail.com>

---
+++ server/resources/button.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/include-internal.jsp"%>
+<script type="text/javascript">
+  $j('.quickLinks').prepend('<a href="#" class="quickLinksItem" onclick="alert(\'Enable!\');">Enable All Agents</a>');
+  $j('.quickLinks').prepend('<a href="#" class="quickLinksItem" onclick="alert(\'Disable!\');">Disable All Agents</a>');
+</script>
---
+++ server/src/disableAllAgents/UiExtension.java
@@ -0,0 +1,55 @@
+package disableAllAgents;
+
+import jetbrains.buildServer.serverSide.auth.Permission;
+import jetbrains.buildServer.serverSide.auth.SecurityContext;
+import jetbrains.buildServer.users.SUser;
+import jetbrains.buildServer.web.openapi.PagePlaces;
+import jetbrains.buildServer.web.openapi.PlaceId;
+import jetbrains.buildServer.web.openapi.PluginDescriptor;
+import jetbrains.buildServer.web.openapi.SimplePageExtension;
+import jetbrains.buildServer.web.util.WebUtil;
+import jetbrains.buildServer.log.Loggers;
+import org.jetbrains.annotations.NotNull;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+public class UiExtension extends SimplePageExtension {
+
+  @NotNull
+  private static final String EXTENSION_INCLUDE_URL = "button.jsp";
+
+  @NotNull
+  private static final String EXTENSION_AVAILABILITY_URL = "/agents.html";
+
+  @NotNull
+  private final SecurityContext mySecurityContext;
+
+  public UiExtension(@NotNull final PagePlaces pagePlaces,
+                     @NotNull final PluginDescriptor descriptor,
+                     @NotNull final SecurityContext securityContext) {
+    super(pagePlaces);
+    mySecurityContext = securityContext;
+    setPlaceId(PlaceId.BEFORE_CONTENT);
+    setPluginName(descriptor.getPluginName());
+    setIncludeUrl(descriptor.getPluginResourcesPath(EXTENSION_INCLUDE_URL));
+    Loggers.SERVER.info("==== Plugin loaded " + descriptor.getPluginName() + "  " + descriptor.getPluginResourcesPath(EXTENSION_INCLUDE_URL));
+  }
+
+  @Override
+  public boolean isAvailable(@NotNull final HttpServletRequest request) {
+    final SUser user = (SUser) mySecurityContext.getAuthorityHolder().getAssociatedUser();
+    return WebUtil.getPathWithoutAuthenticationType(WebUtil.getPathWithoutContext(request, WebUtil.getOriginalRequestUrl(request))).startsWith(EXTENSION_AVAILABILITY_URL)
+            && user != null
+            && user.isPermissionGrantedGlobally(Permission.ENABLE_DISABLE_AGENT);
+  }
+
+  @Override
+  public void fillModel(@NotNull final Map<String, Object> model, @NotNull final HttpServletRequest request) {
+    super.fillModel(model, request);
+  }
+}
+
+
+
+
